6dc80df009310d0bcb6a93e940725324ee68a770
Anton Dobrev <anton.dobrev@telerik.com>
2014-12-01 08:30:42 +0000

    Initial commit of the project.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,57 @@
-(function() {
-    var apiKey = "YOUR API KEY";
+(function () {
+    var apiKey = "YOUR_API_KEY";
     var el = new Everlive(apiKey);
 
-    var groceryDataSource = new kendo.data.DataSource({
-        transport: {
-            read: {
-                url: "https://api.everlive.com/v1/" + apiKey + "/Groceries",
-                dataType: "jsonp"
+    window.addView = kendo.observable({
+        add: function () {
+            if (this.grocery.trim() === "") {
+                navigator.notification.alert("Please provide a grocery.");
+                return;
             }
+
+            groceryDataSource.add({
+                Name: this.grocery
+            });
+            groceryDataSource.one("sync", this.close);
+            groceryDataSource.sync();
         },
-        schema: {
-            data: function(response) {
-                return response.Result;
-            }
+        close: function () {
+            $("#add").data("kendoMobileModalView").close();
+            this.grocery = "";
         }
     });
 
+    var groceryDataSource = new kendo.data.DataSource({
+        offlineStorage: "grocery-storage",
+        type: "everlive",
+        transport: {
+            typeName: "Groceries"
+        }
+    });
+
+    function bindNetworkEvents() {
+        document.addEventListener("online", function() {
+            groceryDataSource.online(true);
+        });
+        
+        document.addEventListener("offline", function() {
+            groceryDataSource.online(false);
+        }); 
+    };
+    
     function initialize() {
         var app = new kendo.mobile.Application(document.body, {
             skin: "flat",
             transition: "slide"
         });
         navigator.splashscreen.hide();
+
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
+        
+        bindNetworkEvents();
     }
 
     document.addEventListener("deviceready", initialize);
